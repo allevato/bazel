66b002c6d2d98fd7a5a4abe1985e15b5dc99e87b
Tony Allevato <tony.allevato@gmail.com>
2017-07-08 09:55:50 -0700

    Add wrappers around the debugger protos.

---
+++ src/main/java/com/google/devtools/build/lib/syntax/debugprotocol/DebugEvent.java
@@ -0,0 +1,28 @@
+// Copyright 2017 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.devtools.build.lib.syntax.debugprotocol;
+
+/** Represents an event or response sent from the debug server to a debugger client. */
+public class DebugEvent {
+  private DebugProtos.DebugEvent eventProto;
+
+  private DebugEvent(DebugProtos.DebugEvent eventProto) {
+    this.eventProto = eventProto;
+  }
+
+  public DebugProtos.DebugEvent asEventProto() {
+    return eventProto;
+  }
+}
